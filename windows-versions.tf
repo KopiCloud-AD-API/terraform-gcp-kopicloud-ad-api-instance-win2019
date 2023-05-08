441827f93a0843a762c9ff3fd95b236adc081254
Guillermo Musumeci <guillermo-musumeci@users.noreply.github.com>
2023-05-08 21:16:40 +0200

    Initial commit

---
+++ windows-versions.tf
@@ -0,0 +1,10 @@
+######################
+## Windows Versions ##
+######################
+
+variable "windows_2019_sku" {
+  type        = string
+  description = "SKU for Windows Server 2019"
+  default     = "windows-cloud/windows-2019"
+}
+
